79bd7efd9c55eedcbcd0940fe75f542e551e46ad
Emerson Junior <emersonjr.eng@gmail.com>
2016-09-26 14:56:12 +0000

    changed address of websocket connection

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-var conn = new WebSocket('ws://10.10.10.3:9090'); 
+var conn = new WebSocket('ws://localhost:9090'); 
 
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
